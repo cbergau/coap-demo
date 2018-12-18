5a71c84e1ce87fd5ebce6a3c2f09a7b74d89884a
Christian Bergau <cbergau86@gmail.com>
2018-12-18 19:35:49 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+**Kompilieren**
+
+    mvn clean package
+    
+**Ausführen unter Windows**
+
+    java -jar .\target\californiumdemo-1.0-SNAPSHOT.jar coap://californium.eclipse.org:5683
---
+++ src/main/java/DemoApplication.java
@@ -0,0 +1,57 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import org.eclipse.californium.core.CoapClient;
+import org.eclipse.californium.core.CoapResponse;
+import org.eclipse.californium.core.Utils;
+
+public class DemoApplication {
+    public static void main(String... args) {
+        URI uri = null; // URI parameter of the request
+
+        if (args.length > 0) {
+
+            // input URI from command line arguments
+            try {
+                uri = new URI(args[0]);
+            } catch (URISyntaxException e) {
+                System.err.println("Invalid URI: " + e.getMessage());
+                System.exit(-1);
+            }
+
+            CoapClient client = new CoapClient(uri);
+            CoapResponse response = client.get();
+
+            if (response != null) {
+
+                System.out.println(response.getCode());
+                System.out.println(response.getOptions());
+                if (args.length > 1) {
+
+                    try (FileOutputStream out = new FileOutputStream(args[1])) {
+                        out.write(response.getPayload());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    System.out.println(response.getResponseText());
+                    System.out.println(System.lineSeparator() + "ADVANCED" + System.lineSeparator());
+                    System.out.println(Utils.prettyPrint(response));
+                }
+            } else {
+                System.out.println("No response received.");
+            }
+
+        } else {
+            // display help
+            System.out.println("Californium (Cf) GET Client");
+            System.out.println("(c) 2014, Institute for Pervasive Computing, ETH Zurich");
+            System.out.println();
+            System.out.println("Usage : " + DemoApplication.class.getSimpleName() + " URI [file]");
+            System.out.println("  URI : The CoAP URI of the remote resource to GET");
+            System.out.println("  file: optional filename to save the received payload");
+        }
+    }
+}
